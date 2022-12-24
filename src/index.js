db974bf5a30ae427b1cd73d805b5b4b48cfcc738
rafopm <rafopm@gmail.com>
2022-12-24 18:09:55 -0500

    finish version 3 responsive

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ function encriptador() {
   var obj = document.querySelector('.texto').innerHTML;
   if (obj === "") alert("Debe ingresar el texto a encriptar.")
   else {
+    obj = obj.toLowerCase();
     var enc = ""
     for (let i = 0; i < obj.length; i++) {
       enc = (vocales[obj[i]]) ? enc += vocales[obj[i]] : enc += obj[i];
@@ -13,9 +14,9 @@ function encriptador() {
     document.getElementById('areaTexto').innerHTML = enc;
     document.querySelector('.texto').innerHTML = "";
   }
-  document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('DOMContentLoaded', function () {
     autosize(document.querySelectorAll('areaTexto'));
-}, false);
+  }, false);
 }
 
 function desencriptador() {
@@ -40,24 +41,14 @@ function soloLetras(e) {
   var key = e.keyCode || e.which,
     tecla = String.fromCharCode(key).toLowerCase(),
     letras = " abcdefghijklmnñopqrstuvwxyz",
-    especiales = [], //[8, 37, 39, 46],
+    //especiales = [], //[8, 37, 39, 46],
     tecla_especial = false;
-
-  for (var i in especiales) {
-    if (key == especiales[i]) {
-      tecla_especial = true;
-      break;
-    }
-  }
-
   if (letras.indexOf(tecla) == -1 && !tecla_especial) {
     return false;
   }
+
 }
 
-document.querySelector('#miTexto').onkeyup = function (e) {
-  this.value = String(this.value || '').toLowerCase();
-};
 
 var start = ['idbtnCopiar', 'areaTexto']
 var end = ['muneco', 'txtPreviusTittle', 'txtPreviusDesc']
